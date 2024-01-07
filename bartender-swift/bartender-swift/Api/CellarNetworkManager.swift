944a466e45dcb021cd1bc0b06d97c5f03a179163
anishagrawal <anishagrawal2003@gmail.com>
2024-01-07 04:24:22 -0800

    before a lot of ssl changes

--- bartender-swift/bartender-swift/Api/CellarNetworkManager.swift
+++ bartender-swift/bartender-swift/Api/CellarNetworkManager.swift
@@ -12,8 +12,7 @@ struct CellarNetworkManager {
     
     var baseURL: String {
         // Get the root URL from the environment variable
-        let rootURL = ProcessInfo.processInfo.environment["ROOT_URL"] ?? "http://defaultroot.com"
-        
+        let rootURL = ConfigurationManager.shared.rootURL ?? "http://defaultroot.com"
         return rootURL + "/api/cellar"
     }
 
--- bartender-swift/bartender-swift/Auxiliary/ConfigurationManager.swift
+++ bartender-swift/bartender-swift/Auxiliary/ConfigurationManager.swift
@@ -13,7 +13,35 @@ class ConfigurationManager {
     private(set) var rootURL: String?
 
     private init() {
-        rootURL = ProcessInfo.processInfo.environment["ROOT_URL"]
+        // Check if we are in DEBUG mode; if so, load from Development.plist, otherwise load from Production.plist.
+        #if DEBUG
+        rootURL = loadRootURL(fromPlistNamed: "Development")
+        #else
+        rootURL = loadRootURL(fromPlistNamed: "Production")
+        #endif
+    }
+    
+    private func loadRootURL(fromPlistNamed plistName: String) -> String? {
+        guard let plistPath = Bundle.main.path(forResource: plistName, ofType: "plist"),
+              let plistDict = NSDictionary(contentsOfFile: plistPath),
+              let url = plistDict["RootURL"] as? String else {
+            return nil
+        }
+        return url
     }
 }
 
+class CustomURLSessionDelegate: NSObject, URLSessionDelegate {
+    func urlSession(_ session: URLSession,
+                    didReceive challenge: URLAuthenticationChallenge,
+                    completionHandler: @escaping (URLSession.AuthChallengeDisposition, URLCredential?) -> Void) {
+        if challenge.protectionSpace.authenticationMethod == NSURLAuthenticationMethodServerTrust,
+           let serverTrust = challenge.protectionSpace.serverTrust,
+           challenge.protectionSpace.host == "bartender.api.anish.xyz" {
+            let credential = URLCredential(trust: serverTrust)
+            completionHandler(.useCredential, credential)
+        } else {
+            completionHandler(.performDefaultHandling, nil)
+        }
+    }
+}
