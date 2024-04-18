732f92d2684bcb2cf457c4a7e320b0e6a34ffcf5
anishagrawal <anishagrawal2003@gmail.com>
2024-04-17 23:44:14 -0400

    create recipe view

--- bartender/bartender/Cellar/EditBottleView.swift
+++ bartender/bartender/Cellar/EditBottleView.swift
@@ -6,7 +6,6 @@
 //
 
 import SwiftUI
-import UIKit
 
 struct EditBottleView: View {
     @Binding var bottle: Bottle
---
+++ bartender/bartender/Cocktails/CreateCocktailRecipeView.swift
@@ -0,0 +1,8 @@
+//
+//  CreateCocktailRecipeView.swift
+//  bartender
+//
+//  Created by Anish Agrawal on 4/17/24.
+//
+
+import Foundation
